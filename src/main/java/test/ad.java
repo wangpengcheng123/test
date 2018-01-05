b146e39a41beab59b99750bf649212ddf4abfb2c
pengchengwang <pengchengwang@192.168.191.3>
2018-01-05 09:20:48 +0800

    sdfsdaf

---
+++ src/main/java/test/ad.java
@@ -0,0 +1,20 @@
+/**
+ * Project Name:test
+ * File Name:ad.java
+ * Package Name:test
+ * Date:2018年1月5日上午9:20:28
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package test;
+/**
+ * Description:   <br/>
+ * Date:     2018年1月5日 上午9:20:28 <br/>
+ * @author   pengchengwang
+ * @version
+ * @see
+ */
+public class ad {
+
+}
+
