1225a48e451b3acb5703b8c69801653d8204e17b
huanglei <huanglei@192.168.191.7>
2018-01-05 09:08:37 +0800

    huangleu

---
+++ huanglei.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name:test
+ * File Name:huanglei.java
+ * Package Name:
+ * Date:2018年1月5日上午9:08:13
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+/**
+ * Description:   <br/>
+ * Date:     2018年1月5日 上午9:08:13 <br/>
+ * @author   huanglei
+ * @version
+ * @see
+ */
+public class huanglei {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
